cde7bf4eb1b687e56cc4609d52f9cebe46089959
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-05 13:03:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,7 @@ class ApplicationController < Sinatra::Base
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      binding.pry
       session[:user_id] = @user.id
       redirect '/users/home'
     end
